6e8291a5fce3b7d8713ac65c0c38580e00ffe471
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-02 15:26:35 -0500

    finish drills

--- drills.js
+++ drills.js
@@ -58,26 +58,114 @@ O(n) - Linear run time complexity. For loop that iterates through each item once
 
 // 11. Tower of Hanoi
 
-let rodA = [];
+let rodA = [3,2,1];
 let rodB = [];
 let rodC = [];
 
-let disk1 = 1;
-let disk2 = 2;
-let disk3 = 3;
 
-
-function towerOfHanoi() {
-
+function towerOfHanoi(disk, start, middle, end) {
+  if (disk === 1) {
+    return;
+  }
+  end.push(start.pop());
+  console.log(rodA, rodB, rodC);
+  towerOfHanoi(disk - 1, start, end, middle);
+  towerOfHanoi(disk - 1, end, start, middle);
+  towerOfHanoi(disk - 1, start, middle, end);
+  towerOfHanoi(disk - 1, middle, end, start);
+  towerOfHanoi(disk - 1, middle, start, end);
+  towerOfHanoi(disk - 1, start, middle, end); 
 }
 
-// iterative approach: 
-// condition to make sure disks cant move if they have disk     on top of them
-// doesnt allow disks to go back to where it was previously
-// base case = when arr3 == initial value arr1
-// insert algorithm here
-// display each movement of the disk with a console.log?
+//towerOfHanoi(3, rodA, rodB, rodC);
+
+
+function countSheep(num) {
+  for (let i = num; i > 0; i--) {
+    if (i >= 1) {
+      console.log('Another sheep jump over the fence');
+    }
+  }
+  console.log('All sheep jumped over the fence');
+}
+// Linear, O(n)
+//countSheep(3);
+
+function powerCalculator(baseInt, expInt) {
+  let result = baseInt;
+
+  for (let i = expInt; i > 1; i--) {
+    result *= baseInt;
+  }
+  return result;
+}
+// Linear, O(n)
+//console.log(powerCalculator(10, 3));
+
+function reverseString(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    // take last letter in string
+    result += str[str.length - i - 1];
+    // replace last letter with first letter
+
+  }
+  return result;
+}
+// Linear, O(n)
+//console.log(reverseString('hello'));
+
+function TriangularNum(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+    console.log(sum);
+  }
+  return sum;
+}
+// Linear, O(n)
+//TriangularNum(5);
+
+function stringSplitter(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== '/') {
+      result += str[i];
+    }
+  }
+  console.log(result);
+  return result;
+}
+// Linear, O(n)
+//stringSplitter('02/20/2020');
+
+function fibonacci(num) {
+  const numArr = [];
+  for (let i = 0; i < num; i++) {
+    if (!numArr.length) {
+      numArr.push(1);
+      numArr.push(i);
+    }
+    numArr.push(numArr[i+1] + numArr[i]);
+  }
+  numArr.splice(0, 2);
+  console.log(numArr);
+}
+// Linear, O(n)
+//fibonacci(7);
 
 
 
-
+/* 
+  Counting Sheep function - Linear, O(n)
+  Power Calculator function - Linear, O(n)
+  ReverseString function - Linear, O(n)
+  Triangular Number function - Linear, O(n)
+  String Splitter function - Linear, O(n)
+  Factorial function - Linear, O(n)
+  Fibonacci function - Linear, O(n)
+  Find Way Out of The Maze function - Linear, O(n)
+  Find ALL the ways out of the maze function - Linear, O(n)
+  Anagramsfunction - Organization Chart function - Linear, O(n)
+  binaryRep function - constant, O(1)
+*/
